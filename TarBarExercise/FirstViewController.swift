27090e8394276be9e57fd05a48255500e723274d
befint1989 <lizongyu@lizongyudemacbook-pro.local>
2016-08-16 09:43:25 +0800

    Initial Commit

---
+++ TarBarExercise/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TarBarExercise
+//
+//  Created by 李宗育 on 2016/8/16.
+//  Copyright © 2016年 李宗育. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TarBarExercise/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TarBarExercise
+//
+//  Created by 李宗育 on 2016/8/16.
+//  Copyright © 2016年 李宗育. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
