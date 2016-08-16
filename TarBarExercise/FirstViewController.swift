109c09bcab1ac1537e803ffbb71d2393b97d36ba
befint1989 <lizongyu@lizongyudemacbook-pro.local>
2016-08-16 10:17:44 +0800

    練習

--- TarBarExercise/FirstViewController.swift
+++ TarBarExercise/FirstViewController.swift
@@ -13,14 +13,16 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var first_title: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("FirstViewController viewDidLoad")
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+        print("FirstViewController viewWillAppear")
         first_title.text = "資料載入中"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
